62a255220993a7be0e2223062f1a8421a79c6499
Keith Hackbarth <keith.hackbarth@gmail.com>
2015-01-29 15:44:38 -0800

    Try explicitly setting django settings

--- clock.py
+++ clock.py
@@ -1,6 +1,10 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from django.core.mail import send_mail
 
+import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'clowder_product.settings'
+
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=3)
