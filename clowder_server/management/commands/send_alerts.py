f8aa722b9b56ca543f73a40f22fd682a1c71fb4c
ParkMe <parkme@ParkMes-MacBook-Pro.local>
2015-03-13 13:25:43 -0700

    Delete old unused pings from users

--- clowder_server/management/commands/send_alerts.py
+++ clowder_server/management/commands/send_alerts.py
@@ -2,13 +2,22 @@ import datetime
 
 from django.core.management.base import BaseCommand, CommandError
 
+from clowder_account.models import ClowderUser
 from clowder_server.emailer import send_alert
-from clowder_server.models import Alert
+from clowder_server.models import Alert, Ping
 
 class Command(BaseCommand):
     help = 'Checks and sends alerts'
 
     def handle(self, *args, **options):
+
+        # delete old pings
+        for user in ClowderUser.objects.all():
+            pings = Ping.objects.filter(user=user)[:500]
+            pings = list(pings) # forces database hit
+            Ping.objects.exclude(pk__in=pings).delete()
+
+        # send alerts
         alerts = Alert.objects.filter(notify_at__lte=datetime.datetime.now)
         for alert in alerts:
             send_alert(request.user, alert.name)
