3880918d6e9b9bb96675489bb7564ddd6cb649cd
Eric Scrivner <eric.t.scrivner@gmail.com>
2015-06-23 14:41:06 -0700

    Add Missing Import

--- clowder_account/models.py
+++ clowder_account/models.py
@@ -2,6 +2,7 @@ from shortuuidfield import ShortUUIDField
 
 from django.contrib import admin
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.mail import send_mail
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
