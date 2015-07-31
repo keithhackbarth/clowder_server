fe6da9ee88a9a7e55569f74761511bd20687dcbc
Keith Hackbarth <keith.hackbarth@gmail.com>
2015-07-31 15:44:54 -0700

    COMPANY: Fix new user sign-up

--- clowder_account/views.py
+++ clowder_account/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login
 from django.views.generic.base import RedirectView
 
-from clowder_account.models import ClowderUser
+from clowder_account.models import Company, ClowderUser
 
 class RegisterView(RedirectView):
 
@@ -12,10 +12,14 @@ class RegisterView(RedirectView):
         email = request.POST['email']
         password = request.POST['password']
 
+        company = Company()
+        company.save()
+
         user = ClowderUser.objects.create_user(email, password)
         user.first_name = request.POST['first_name']
         user.last_name = request.POST['last_name']
         user.language = request.POST['language']
+        user.company = company
         user.save()
 
         user = authenticate(username=email, password=password)
