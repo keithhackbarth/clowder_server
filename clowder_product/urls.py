1bb197257e386e9697749a0ad9807d6aa9a0cdb2
Keith Hackbarth <keith.hackbarth@gmail.com>
2015-09-22 09:29:32 -0700

    Add about page to Clowder

--- clowder_product/urls.py
+++ clowder_product/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
 
     url(r'', include('clowder_account.urls')),
     url(r'', include('clowder_server.urls')),
+    url(r'^about/', TemplateView.as_view(template_name='about.html')),
     url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^admin/', include(admin.site.urls)),
 )
