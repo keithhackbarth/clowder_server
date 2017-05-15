1afe2aec9c101647bea29af36e689d008223bb01
geekman2 <geekmann2@gmail.com>
2017-05-15 11:38:04 -0700

    Lock slack messages to Parkme only

--- clowder_server/emailer.py
+++ clowder_server/emailer.py
@@ -10,8 +10,9 @@ def send_alert(company, name):
     for user in ClowderUser.objects.filter(company=company):
         subject = 'FAILURE: %s' % (name)
         body = subject
-        slack_token = os.getenv('PARKME_SLACK_TOKEN')
-        url = 'https://hooks.slack.com/services/%s' % (slack_token)
-        payload = {"username": "devopsbot", "text": body, "icon_emoji": ":robot_face:"}
-        requests.post(url, json=payload)
+        if user.company_id == 86:
+            slack_token = os.getenv('PARKME_SLACK_TOKEN')
+            url = 'https://hooks.slack.com/services/%s' % (slack_token)
+            payload = {"username": "devopsbot", "text": body, "icon_emoji": ":robot_face:"}
+            requests.post(url, json=payload)
         send_mail(subject, body, ADMIN_EMAIL, [user.email], fail_silently=True)
