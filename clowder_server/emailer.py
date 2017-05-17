3313d611d7cc66bf607a341a5d9a6a5d96dfbec5
geekman2 <geekmann2@gmail.com>
2017-05-17 11:46:49 -0700

    Rename bot and prevent channel spamming

--- clowder_server/emailer.py
+++ clowder_server/emailer.py
@@ -7,12 +7,14 @@ from clowder_account.models import ClowderUser
 ADMIN_EMAIL = 'admin@clowder.io'
 
 def send_alert(company, name):
+    slack_sent = False
     for user in ClowderUser.objects.filter(company=company, allow_email_notifications=True):
         subject = 'FAILURE: %s' % (name)
         body = subject
-        if user.company_id == 86:
+        if user.company_id == 86 and not slack_sent:
             slack_token = os.getenv('PARKME_SLACK_TOKEN')
             url = 'https://hooks.slack.com/services/%s' % (slack_token)
-            payload = {"username": "devopsbot", "text": body, "icon_emoji": ":robot_face:"}
+            payload = {"username": "clowder", "text": body, "icon_emoji": ":clowder:"}
             requests.post(url, json=payload)
+            slack_sent = True
         send_mail(subject, body, ADMIN_EMAIL, [user.email], fail_silently=True)
